4c664a669d9ffdf6eb9e10f6daff3b2e4843e580
vshivakumar55 <129029164+vshivakumar55@users.noreply.github.com>
2025-07-09 12:24:15 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
+from flask import Flask
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Loads the .env file
+load_dotenv()  # Load .env file
+
+app = Flask(__name__)
 
 mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri)
+
+db = client.webhook_data
+collection = db.events
